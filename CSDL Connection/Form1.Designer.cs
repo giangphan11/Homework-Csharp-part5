96f972adad08f11984d4cb726c132491c83ff896
Giang Phan <gphan485@gmail.com>
2020-04-15 09:54:21 +0700

    test connect SQL Server

--- CSDL Connection/Form1.Designer.cs
+++ CSDL Connection/Form1.Designer.cs
@@ -28,13 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(139, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kết nối CSDL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(139, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(348, 48);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Ngắt kết nối CSDL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(676, 418);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- CSDL Connection/Form1.cs
+++ CSDL Connection/Form1.cs
@@ -7,14 +7,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CSDL_Connection
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn = null;
+        string strConnection = "SERVER= GIANGPHAN; DATABASE=QuanLySanPham; User=sa; Password=20061998";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(strConnection);
+                conn.Open();
+                MessageBox.Show("Kết nối CSDL thành công !");
+                    }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(conn!=null && conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+                MessageBox.Show("Ngắt kết nối CSDL thành công !");
+            }
         }
     }
 }
